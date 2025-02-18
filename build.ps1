6cc0caf4017b820907d4673898dc304857421b5f
Eisai <12467320+Eisaichen@users.noreply.github.com>
2025-02-18 13:22:27 -0600

    Add ltsc2025 to build.

--- build.ps1
+++ build.ps1
@@ -1,12 +1,31 @@
+# Download Jellyfin
+
 .\wget --no-hsts -q "https://github.com/ollama/ollama/releases/download/$env:GH_CI_TAG/ollama-windows-amd64.zip" -O .\ollama-windows-amd64.zip
 Expand-Archive -Path .\ollama-windows-amd64.zip -DestinationPath .\build\ollama-windows-amd64
 
-docker pull mcr.microsoft.com/windows/servercore:ltsc2022
+
+# Build ltsc2022
+
 if ($env:GH_CI_LATEST -eq "true") {
-    docker build --isolation hyperv --no-cache -t eisai/ollama:latest -t eisai/ollama:$env:GH_CI_TAG .\build
+    docker build --isolation hyperv --no-cache --pull -t eisai/ollama:latest -t eisai/ollama:$env:GH_CI_TAG .\build
 } else {
-    docker build --isolation hyperv --no-cache -t eisai/ollama:$env:GH_CI_TAG .\build
+    docker build --isolation hyperv --no-cache --pull -t eisai/ollama:$env:GH_CI_TAG .\build
 }
+
+
+# Build ltsc2025
+
+$i=Get-Content -Path .\build\Dockerfile
+Set-Content -Path .\Dockerfile -Value $($i.replace("FROM mcr.microsoft.com/windows/servercore:ltsc2022","FROM mcr.microsoft.com/windows/servercore:ltsc2025"))
+
+if ($env:GH_CI_LATEST -eq "true") {
+    docker build --isolation hyperv --no-cache --pull -t eisai/ollama:ltsc2025 -t eisai/ollama:$env:GH_CI_TAG-ltsc2025 .\build
+} else {
+    docker build --isolation hyperv --no-cache --pull -t eisai/ollama:$env:GH_CI_TAG-ltsc2025 .\build
+}
+
+
+# Push
 
 if ($env:GH_CI_PUSH -eq "true") {
     docker push eisai/ollama -a
